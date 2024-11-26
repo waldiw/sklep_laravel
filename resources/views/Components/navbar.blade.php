ff429c010977a5a81fc44ebf3b42ce0c41e54df1
Waldemar Wilke <wwilke@wp.pl>
2024-11-26 06:09:36 +0100

    start mobil

--- resources/views/Components/navbar.blade.php
+++ resources/views/Components/navbar.blade.php
@@ -1,5 +1,20 @@
 <div class="navbarShop sticky">
     <nav class="nav-shop">
+        <div class="koszykM">
+            <a href="{{ route('cart') }}">
+                <div class="koszyk">
+                    Koszyk&nbsp;
+                    <i class="fa-solid fa-cart-shopping" style="color: #ffffff;"></i>
+                    <span class="basketItemCount"></span>
+                </div>
+            </a>
+        </div>
+        <!-- Checkbox for toggling menu -->
+        <input type="checkbox" id="checkM">
+        <!-- Menu icon -->
+        <label for="checkM" class="checkbtn">
+            <i class="fas fa-bars"></i>
+        </label>
         <ul>
             <li>
                 <a href="https://osmolecko.pl">O nas</a>
